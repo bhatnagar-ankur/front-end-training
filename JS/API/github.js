e9106481241240c3151faea22910ec38d757d20b
bhatnagar_ankur <bhatnagar018@gmail.com>
2025-09-16 16:57:15 +0530

    Remove token

--- JS/API/github.js
+++ JS/API/github.js
@@ -1,10 +1,5 @@
-// Example with a hypothetical API that requires an API key
-// const apiKey = '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';
-
-const apiKey = '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';
-
 // Store your token securely (never hardcode in production)
-const githubToken = apiKey;
+const githubToken = 'apiKey';
 
 // Example: Get authenticated user information
 fetch('https://api.github.com/user', {
